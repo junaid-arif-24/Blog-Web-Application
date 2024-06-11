f70f2b9de4e61aaf3d6ec763c0e7a850b1744d30
Junaid Arif <mohdjunaid7078@gmail.com>
2024-06-11 13:11:53 +0530

    create mongodb instance

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 const app = express()
 const port = 3000
 
+mongoose.connect(process.env.MONGOURL).then(()=>{
+    console.log("MongoDB is connected");
+})
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
