5c1e2c2b0b4736e8d7a4ea6d6612c13605c57375
Junaid Arif <mohdjunaid7078@gmail.com>
2024-06-11 16:44:56 +0530

    Update auth controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,6 +6,7 @@ export const signup = async (req, res, next) => {
 
     if (!username || !email || !password || username === "" || email === "" || password === "") {
         next(errorHandler(400, "All fields are required"));
+        return;
     }
 
 
